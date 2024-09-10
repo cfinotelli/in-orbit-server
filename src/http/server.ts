227405a6484de14561a3ff847f18d4340431c029
Carlos Finotelli <carlos.finotelli@outlook.com>
2024-09-10 14:47:32 -0300

    feat: create route to create-goal

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,37 @@
 import fastify from "fastify"
+import { createGoal } from "../features/create-goal";
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from "zod";
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post('/goals', {
+  schema: {
+    body: z.object({
+      title: z.string(),
+      desiredWeeklyFrequency: z.number()
+    })
+  }
+}, async (request, reply) => {
+
+  const { title, desiredWeeklyFrequency } = request.body
+
+  try {
+    const result = await createGoal({
+      title,
+      desiredWeeklyFrequency
+    })
+
+    return reply.status(201).send(result.goal)
+  } catch (error) {
+    return reply.status(
+      400
+    ).send({ "Erro": error })
+  }
+})
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server is running on port http://localhost:3333");
