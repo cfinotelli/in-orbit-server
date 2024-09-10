4f44456dccb89d8c0a92394b2b77f52d3adf9736
Carlos Finotelli <carlos.finotelli@outlook.com>
2024-09-10 15:52:25 -0300

    fix: ensure be a select by goalId

--- src/features/get-week-pending-goals.ts
+++ src/features/get-week-pending-goals.ts
@@ -4,7 +4,7 @@ import { goalCompletions, goals } from "../db/schema";
 import { and, count, eq, gte, lte, sql } from "drizzle-orm";
 
 export async function getWeekPendingGoals() {
-  const fisrtdayOfWeek = dayjs().startOf('week').toDate()
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
   const lastDayOfWeek = dayjs().endOf('week').toDate()
 
   const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
@@ -21,13 +21,13 @@ export async function getWeekPendingGoals() {
   const goalCompletionCounts = db.$with('goal_completion_counts').as(
     db
       .select({
-        goalId: goalCompletions.id,
+        goalId: goalCompletions.goalId,
         completionCount: count(goalCompletions.id).as('completionCount')
       })
       .from(goalCompletions)
       .where(
         and(
-          gte(goalCompletions.createdAt, fisrtdayOfWeek),
+          gte(goalCompletions.createdAt, firstDayOfWeek),
           lte(goalCompletions.createdAt, lastDayOfWeek)
         ))
       .groupBy(goalCompletions.goalId)
